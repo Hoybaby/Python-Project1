0ebf8a6f2256a28995eaae978aa051756788d57b
Hoybaby <mbartek436@gmail.com>
2021-03-13 23:37:15 -0500

    making some more pages for a login which is using Jinja to pull from the main html and then creating some javascript for log in and logout

---
+++ app/assets/javascript/login.js
@@ -0,0 +1,18 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            header
+        }) 
+    }
+
+}
