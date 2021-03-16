b1c8061c5d2947a0caa6f54a7d79a925a79f009b
Hoybaby <mbartek436@gmail.com>
2021-03-16 12:26:10 -0400

    adding routes to the mutiple pages with Blueprint and assigning that value with render_template so that when it returns, it is not a simple string. The BP.route is like router in node.js

---
+++ app/routes/__init__.py
@@ -0,0 +1,3 @@
+from .home import bp as home
+
+# the .home syntax direct the program to find the module name home then import BP routes.
---
+++ app/routes/home.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, render_template
+
+# Blueprint lets us consolidate routes into a dingle bp object and that the parent app can register later. Very similar to Router in express
+
+bp = Blueprint('home', __name__, url_prefix='/')
+
+@bp.route('/')
+def index():
+    return render_template('homepage.html')
+
+@bp.route('/login')
+def login():
+    return render_template('login.html')
+
+@bp.route('/post/<id>')
+def single(id):
+    return render_template('single-post.html')
