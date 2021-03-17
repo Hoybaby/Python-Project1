dea6c4fa6aac466ef1882a6c28b7320e753e8b10
Hoybaby <mbartek436@gmail.com>
2021-03-17 00:34:12 -0400

    my route to dashboard wasnt working properly because I had an issue in my home.py file where the prefix was wrong and then properly routed in the init.py in routes and in the app itself

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 # this bp will be different url_prefix because i want it to go to dashboard html page
 # once again, this file is a module so every variable or function that belongs to this module can be imported elsewhere.
-bp = Blueprint('home', __name__, url_prefix='/dashboard')
+bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
 @bp.route('/')
 def dash():
