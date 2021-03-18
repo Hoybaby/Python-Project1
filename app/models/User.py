45d3e54771ae28420726711145e5af18060f51c2
Hoybaby <mbartek436@gmail.com>
2021-03-17 20:51:02 -0400

    made use of several varibales to maanage the connection to database and session genterates temporany connections while perfoming, create, read and update CRUD operations. We then imported Base to the models package to use it in a class of User which the class will use to make a table

---
+++ app/models/User.py
@@ -0,0 +1,11 @@
+from app.db import Base
+from sqlaclchemy import Column, Integer, String
+
+# created a User class that inherits from the Base Class
+# the Base class variables helps us map the models to real MySQL tables which is key
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable =False)
+    email = Column(String(50), nullable=False, unique=True)
+    password= Column(string(50), nullable=False)
