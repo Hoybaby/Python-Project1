d003aba6db8bdcc296374fa07b46bbe39ebc3361
Hoybaby <mbartek436@gmail.com>
2021-03-19 00:07:23 -0400

    making the email validation and password validation with requirements for the password"

--- app/models/User.py
+++ app/models/User.py
@@ -1,6 +1,6 @@
 from app.db import Base
 from sqlalchemy import Column, Integer, String
-
+from sqlalchemy.orm import validates
 
 # created a User class that inherits from the Base Class
 # the Base class variables helps us map the models to real MySQL tables which is key
@@ -12,3 +12,18 @@ class User(Base):
     username = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False, unique=True)
     password = Column(String(100), nullable=False)
+
+# the @validates('') targets the value of the column i am targeting
+# the assert is basically the required in the html part where it needs it to function the return
+@validates('email')
+def validate_email(self, key, email):
+    # make sure email address contains a @ character
+    assert '@' in email
+
+    return email
+
+@validates('password')
+def validate_password(self, key, password):
+    assert len(password > 4)
+
+    return password
