6bfb17aa5e532db421b092620ec044ecf66c78d9
Hoybaby <mbartek436@gmail.com>
2021-03-19 20:50:25 -0400

    was able to create a new table for the posts and have it have the id proper correctly

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,15 +1,17 @@
 from datetime import datetime
 from app.db import Base
-from sqlalchemy import Column, Integer, String, ForeignKey, datetime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 
 # just like the user.py class to make a model properly for the table, need to make a python class.
 
-class Post(base):
+class Post(Base):
     __tablename__='posts'
     id = Column(Integer, primary_key=True)
     title = Column(String(100), nullable=False)
     post_url = Column(String(100), nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'))
     created_at = Column(DateTime, default = datetime.now)
-    updated_at = Column(DateTime, default = datetime.now)
+    updated_at = Column(DateTime, default = datetime.now, onupdate=datetime.now)
+    user = relationship('User')
+    # In the SQLAlchemy model, we can define dynamic properties that won't become part of the MySQL table but that the query will return.
