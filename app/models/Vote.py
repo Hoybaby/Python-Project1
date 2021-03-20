f6856ec9e70c96e883a77a9839b41ff28d9cfa4f
Hoybaby <mbartek436@gmail.com>
2021-03-20 13:16:40 -0400

    the vote system for the table is very simple so doesnt need much but it will update to the post model too so more needs to be added

---
+++ app/models/Vote.py
@@ -0,0 +1,8 @@
+from app.db import Base
+from sqlalchemy import Column, Integer, ForeignKey
+
+class Vote(Base):
+    __table__name: 'votes'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.is'))
