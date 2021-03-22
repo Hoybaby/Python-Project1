fb56a350a51cc5738509beafae2db6098f9229e3
Hoybaby <mbartek436@gmail.com>
2021-03-22 01:12:56 -0400

    fixed a type i had the wasnt running my program on flask so i fixed it an also added more code to my function signup to request it.

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,11 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from app.models import User
 from app.db import get_db
 
-bp = Blueprint('api', __name__, url_prefrix='/api')
+bp = Blueprint('api', __name__, url_prefix='/api')
 
 # we add a new route that will resole to /api/users and specified the method to be POST
 # purpose of a POST route is to receive data but then using, request from flask to retrieve it
-@bp.route('/users, methods=['POST'])
+@bp.route('/users', methods=['POST'])
 def signup():
+    data = request.get_json()
+    print(data)
+
     return ''
