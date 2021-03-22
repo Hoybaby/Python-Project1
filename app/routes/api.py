f0b143a502d1df9df98b4bf84e718e2020572eff
Hoybaby <mbartek436@gmail.com>
2021-03-22 13:31:01 -0400

    created the route for comments. first set up the database connection with lines 79 and 80 then did a try and except comment to make sure i have a back up system in place.

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify, session
-from app.models import User
+from app.models import User, Post, Comment, Vote
 from app.db import get_db
 import sys
 
@@ -71,3 +71,26 @@ def login():
     session['loggedIn'] = True
 
     return jsonify(id = user.id)
+
+# ROUTE FOR COMMENTS. very similar to the login route we made on line 11
+
+@bp.route('/comments', methods=['POST'])
+def comment():
+    data = request.get_json()
+    db = get_db()
+    # this part takes care of connecting to the database
+    try:
+    # create a new comment
+        newComment = Comment(
+            comment_text = data['comment_text'],
+            post_id = data['post_id'],
+            user_id = session.get('user_id')
+        )
+        db.add(newComment)
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
+        db.rollback()
+        return jsonify(message = 'Comment failed'), 500
+        
+    return jsonify(id = newComment.id)
