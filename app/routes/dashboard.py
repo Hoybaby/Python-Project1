53f8470a798c4de97afc49d3deaf2ef955e35d23
Hoybaby <mbartek436@gmail.com>
2021-03-23 13:41:30 -0400

    i wrapped my function to require logged in to see the specfic paramters. Also protected the function with a decorator.

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,12 +1,14 @@
 from flask import Blueprint, render_template, session
 from app.models import Post
 from app.db import get_db
+from app.utils.auth import login_required
 
 # this bp will be different url_prefix because i want it to go to dashboard html page
 # once again, this file is a module so every variable or function that belongs to this module can be imported elsewhere.
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
 @bp.route('/')
+@login_required
 def dash():
     db = get_db()
     posts = (   
@@ -23,6 +25,7 @@ def dash():
 
 
 @bp.route('/edit/<id>')
+@login_required
 def edit(id):
     # get single post by id
     db = get_db()
